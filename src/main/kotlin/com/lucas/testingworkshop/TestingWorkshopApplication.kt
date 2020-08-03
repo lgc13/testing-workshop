cf0a991a49ead2805ed5367cf6072289266e38a8
Lucas Costa <lucas_costa@unigroup.com>
2020-08-03 09:46:48 -0500

    initial commit

---
+++ src/main/kotlin/com/lucas/testingworkshop/TestingWorkshopApplication.kt
@@ -0,0 +1,11 @@
+package com.lucas.testingworkshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestingWorkshopApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestingWorkshopApplication>(*args)
+}
---
+++ src/test/kotlin/com/lucas/testingworkshop/TestingWorkshopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lucas.testingworkshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestingWorkshopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
